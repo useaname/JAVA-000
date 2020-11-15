5e80f510dcfa2bfabc27bf8b41d948ebb6ebceb5
xiaoqi.cheng <wleexi@gmail.com>
2020-11-16 00:56:52 +0800

    [update] 更新 start 作业

--- Week_05/homework-springboot-start/src/main/java/xiaoqi/springboot/start/HomeworkAutoConfiguration.java
+++ Week_05/homework-springboot-start/src/main/java/xiaoqi/springboot/start/HomeworkAutoConfiguration.java
@@ -1,9 +1,11 @@
 package xiaoqi.springboot.start;
 
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnNotWebApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnResource;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +13,10 @@ import org.springframework.context.annotation.Configuration;
  * Homework 自动装配
  */
 @Configuration
+@AutoConfigureBefore(Student.class)
 @ConditionalOnProperty(prefix = "homework", name = "enabled", havingValue = "true", matchIfMissing = true)
 @ConditionalOnResource(resources = "META-INF/spring.factories")
-@ConditionalOnNotWebApplication
-@ConditionalOnExpression("${homework.enabled:true}")
+@EnableConfigurationProperties(Student.class)
 public class HomeworkAutoConfiguration {
 
     /**
--- Week_05/homework-springboot-start/src/main/java/xiaoqi/springboot/start/Student.java
+++ Week_05/homework-springboot-start/src/main/java/xiaoqi/springboot/start/Student.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.io.Serializable;
 
@@ -13,6 +14,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@ConfigurationProperties(prefix = "student")
 public class Student implements Serializable {
     
     private int id;
